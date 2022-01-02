13908f378aa3b97244f2b32f17916b0ac46d509f
JakubDzi <01132920@pw.edu.pl>
2022-01-02 19:13:50 +0100

    lab8

---
+++ laby/lab8/multiscrape.py
@@ -0,0 +1,25 @@
+import requests
+import os
+from PIL import Image
+from bs4 import BeautifulSoup
+from concurrent.futures import ProcessPoolExecutor
+
+def improc(name):
+    print(name + " na procesie nr {}".format(os.getpid()))
+    src = "http://if.pw.edu.pl/~mrow/dyd/wdprir/" + name
+    img_data = requests.get(src).content
+    with open(name, 'wb') as handler:
+        handler.write(img_data)
+    img = Image.open(name)
+    imgGray = img.convert('L')
+    imgGray.save(name)
+
+if __name__=='__main__':
+    req = requests.get('http://if.pw.edu.pl/~mrow/dyd/wdprir/')
+    soup = BeautifulSoup(req.text, 'html.parser')
+    alist = soup.find_all('a')
+    pool = ProcessPoolExecutor(max_workers=12)
+    tasks = []
+    for item in alist:
+        if ".png" in item.get_text():
+            tasks.append(pool.submit(improc,(item.get_text())))
