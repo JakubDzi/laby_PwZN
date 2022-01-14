814d60e1a74e678a15a4f4c76828ec68b2b875b8
JakubDzi <01132920@pw.edu.pl>
2022-01-14 13:27:07 +0100

    lab9

--- laby/lab9/vib_dow.py
+++ laby/lab9/vib_dow.py
@@ -6,10 +6,11 @@ async def main():
 
     async with aiohttp.ClientSession() as session:
 
-        pokemon_url = 'https://pokeapi.co/api/v2/pokemon/1'
-        async with session.get(pokemon_url) as resp:
-            pokemon = await resp.json()
-            print(pokemon['name'])
+        pokemon_url = 'https://pokeapi.co/api/v2/pokemon/'
+        for i in range (10):
+            async with session.get((pokemon_url+str(i+1))) as resp:
+                pokemon = await resp.json()
+                print(pokemon['name'])
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 asyncio.run(main())
